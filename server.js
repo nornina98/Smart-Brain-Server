b5d23df2ba2c2990e571d9c5a15c43f89e73b043
Nor Ikram <nornina8998@gmail.com>
2022-07-20 11:39:32 +0800

    added prettier extension to make more readable

--- server.js
+++ server.js
@@ -1,56 +1,58 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.json());
 
 const database = {
-    users: [
-        {
-            id: '001',
-            name: 'kasyaf',
-            email: 'kasyaf1234@gmail.com',
-            password: 'kasyaf1234',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '002',
-            name: 'muya',
-            email: 'muya1234@gmail.com',
-            password: 'muya1234',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-}
+  users: [
+    {
+      id: "001",
+      name: "kasyaf",
+      email: "kasyaf1234@gmail.com",
+      password: "kasyaf1234",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "002",
+      name: "muya",
+      email: "muya1234@gmail.com",
+      password: "muya1234",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
 
-app.get('/', (req, res) => { 
-    res.send(database.users);
-})
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
 
-app.post('/signin', (req, res) =>{
-    if (req.body.email === database.users[0].email && 
-        req.body.password === database.users[0].password) {
-            res.json('success');
-        } else {
-            res.status(400).json('error to login');
-        }
-})
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error to login");
+  }
+});
 
-app.post('/register', (req, res) => {
-    const {email, name, password} = req.body;
-    database.users.push({
-        id: '003',
-        name: name,
-        email: email,
-        password: password,
-        entries: 0,
-        joined: new Date()
-    })
-    res.json(database.users[database.users.length-1]);
-})
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: "003",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
+});
 
 app.listen(3000, () => {
-    console.log('app working');   
-})
+  console.log("app working and running on server 3000");
+});
